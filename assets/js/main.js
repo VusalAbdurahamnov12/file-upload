64a60f36893031c260e1f2493f974e160b556f85
unknown <Administrator@DESKTOP-GE46997.aztu.local>
2022-05-12 05:32:02 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+let x = document.querySelector("input");
+
+let ctn = document.querySelector(".icn");
+
+ctn.addEventListener("click", function () {
+    x.click();
+})
+
+let imgs = document.querySelector(".img");
+
+x.addEventListener("change", function () {
+    [...this.files].forEach(x => {
+        let filere = new FileReader();
+        if((x.size/1024).toFixed(2) <5000)
+        {
+            let div = document.createElement("div");
+            imgs.appendChild(div);
+            filere.onload = function () {
+                div.innerHTML = "<img src='" + this.result + "'> <span>X</span>";
+                let y = document.querySelectorAll("span")
+                y.forEach(e => {
+                    e.addEventListener("click", function () {
+                        this.parentElement.remove();
+                    })
+                })
+            }
+            filere.readAsDataURL(x);
+
+        }else{
+            alert('5mb kecdunuz')
+        }
+    })
+})
